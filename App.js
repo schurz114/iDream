78f6a167e9c763e52ba52f6c33945342cdea2bc2
schurz114 <schurz114@hotmail.com>
2025-01-07 18:09:55 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>欢迎使用我的Expo应用！</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}); 
